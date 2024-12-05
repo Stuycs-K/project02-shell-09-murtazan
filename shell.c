475863001175de916e6ed999de233a84c590aa37
Naf (Nafiyu Murtaza) <nmurtaza50@stuy.edu>
2024-12-05 14:39:36 -0500

    better parsing system framework implemented, NOT WORKING VER.

--- shell.c
+++ shell.c
@@ -11,7 +11,6 @@
 #include <stdbool.h>
 
 // stuff to do:
-// - fix the two bugs listed on README.md
 // - reorganize the entire code to be in seperate files (one file for parsing? one for all the commands? you can figure it out)
 // - extension of above, remake parsing in order to be able to parse multi-commands and multi-arguments better.
 // - fork and exec [higher priority?]
@@ -33,7 +32,6 @@ void lsCmd(char * cmdInput){ //from lab08
     strncpy(input, cmdInput, inputLen - 1);
     input[inputLen - 1] = '\0';
   }
-  //null terminate the new copy
   //set dir
   DIR * d;
   DIR * d2;
@@ -108,17 +106,33 @@ void cdCmd(char * cmdInput){
 }
 
 void processInput(char * input_buff){
+    char * args[100];
     char * src = input_buff;
     char * cmd;
-    char * arg1;
-    cmd = strsep(&src, " ");
-    arg1 = strsep(&src, " ");
-    if (strncmp("ls", cmd, 2) == 0){
-      lsCmd(arg1);
+    cmd = strsep(&src, ";");
+    int argsIndex = 0;
+    while (cmd != NULL){
+        args[argsIndex] = cmd; //*cmd?? might not work
+        argsIndex += 1;
+        cmd = strsep(&src, ";");
     }
-    if (strncmp("cd", cmd, 2) == 0){
-      cdCmd(arg1);
+    int testIndex = 0;
+    printf("Commands found:\n");
+    while (testIndex < argsIndex){
+        printf("%s\n", args[testIndex]);
+        testIndex += 1;
     }
+    // cmd = strsep(&src, " ");
+    // arg1 = strsep(&src, " ");
+    // if (strncmp("ls", cmd, 2) == 0){
+    //   lsCmd(arg1);
+    // }
+    // if (strncmp("cd", cmd, 2) == 0){
+    //   cdCmd(arg1);
+    // }
+    // if (strncmp("exit", cmd, 4) == 0){
+    //     exit(1);
+    // }
 }
 
 int main(){
